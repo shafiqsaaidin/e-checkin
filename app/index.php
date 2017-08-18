dbe0952bb5776c0fcd31ffa0dc0304d3033625ed
musha <mushagh@gmail.com>
2017-08-18 17:30:02 +0800

    change $_POST to $_REQUEST

--- app/index.php
+++ app/index.php
@@ -1,14 +1,19 @@
 <?php
     require '../connection.php';
 
-    //$ic = mysqli_real_escape_string($conn, $_GET['data']);
-    //$ic = '960117025308';
-    $ic = '960117025307';
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    $ic = mysqli_real_escape_string($conn, $_REQUEST['data']);
+
     // Query set for mysql
     $sql = "SELECT * FROM user WHERE ic=$ic";
 
     $sql1 = "INSERT INTO public(ic, masuk, keluar, tarikh, status)
-        VALUES($ic, '', date_format(now(), '%r'), date_format(now(), '%d-%m-%Y'), 'pending')";
+        VALUES($ic, '', date_format(now(), '%r'), date_format(now(), '%d/%m/%Y'), 'pending')";
 
     $sql2 = "UPDATE public SET masuk=date_format(now(), '%r') WHERE ic=$ic AND masuk='' AND status='approve'";
 
@@ -21,23 +26,23 @@
 
     // Check user ic in table user, if user not exist, need to register first then will insert into public_db
     if($check_row_user > 0){
-        //echo "User available";
-       if($check_row_public > 0 ) {
-           //echo "User exists";
-            if(mysqli_query($conn, $sql2)){
-                echo "Update successfully";
-            } else{
-                echo "Error: ".$sql2."<br>".mysqli_error($conn);
-            }
-       } else {
-            if(mysqli_query($conn, $sql1)){
-                echo "New record created successfully";
-            } else{
-                echo "Error: ".$sql1."<br>".mysqli_error($conn);
-            }
+      //echo "User available";
+     if($check_row_public > 0 ) {
+       //echo "User exists";
+        if(mysqli_query($conn, $sql2)){
+            echo "Update successfully";
+        } else{
+            echo "Error: ".$sql2."<br>".mysqli_error($conn);
         }
+     } else {
+        if(mysqli_query($conn, $sql1)){
+            echo "New record created successfully";
+        } else{
+            echo "Error: ".$sql1."<br>".mysqli_error($conn);
+        }
+      }
     } else {
-        echo "Please register first";
+      echo "Please register first";
     }
 
     mysqli_close($conn);
